235afb7a9a2cc0245f9193ef0e3869312d7595eb
LISA_COOK <lisabpink@gmail.com>
2020-03-13 11:44:42 -0400

    yarn create react app

---
+++ node-api-challenge-stretch/src/components/AppRouter.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Header from "./components/Header.js.js";
+
+import AppRouter from "./components/AppRouter.js.js";
+
+export default function App() {
+  return (
+    <main>
+      <Header />
+      
+      <AppRouter />
+    
+    </main>
+  );
+}
